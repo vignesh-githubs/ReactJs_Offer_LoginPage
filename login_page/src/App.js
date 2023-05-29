6c91e89d68a011ca4eb47c80c990287334e2bf43
vigneshwar <vigneshwar@forushealth.com>
2023-05-29 14:12:12 +0530

    [Added]: developed a project as per requirements

---
+++ login_page/src/App.js
@@ -0,0 +1,141 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='header'>
+        <div className='header-about'>
+          <div className='edyoda'>EDYODA</div>
+          <div className='header-padding'>Courses <i className="arrow down"></i></div>
+          <div className='header-padding'>Programs <i className="arrow down"></i></div>
+        </div>
+        <div className='header-singin'>
+          <div className='header-padding'>search</div>
+          <div className='header-padding'>Log in</div>
+          <div><input type='button' className='join-button'value='JOIN NOW'/></div>
+        </div>
+      </div>
+      <div className='main-content'>
+        <div className='courseinfo '>
+          <div className='courseinfo-header'>
+            <div>Acess curated course worth</div>
+            <div>&#8377;<strike className='striking'><span className='number-color'>18500</span></strike> at just &#8377; <span  className='content-blue-section'> 99</span> per year!
+            </div>
+          </div>
+          <div className='courseinfo-content'>
+            <div><span className='content-blue-section'>100+ </span>Job relevant course</div>
+            <div><span  className='content-blue-section'>20,000+</span> Hours of content</div>
+            <div><span  className='content-blue-section'>Exclusive </span>webinar access</div>
+            <div>Scholarship worth <span  className='content-blue-section'> &#8377;94,500</span></div>
+            <div><span  className='content-blue-section'>Ad Free </span> learning experience</div>
+          </div>
+          </div>
+          <div className='form'>
+           <div className='form-block'>
+            <div className='form-input-boxes'>
+            <div className='input-box number no-border'>
+              <div className='number-pack'>
+              <div className='circle'>1</div>
+              <div className='circle-text'>Sign Up</div>
+              </div>
+              <div className='number-pack'>
+              <div className='circle'>2</div>
+              <div  className='circle-text'>Subscribe</div>
+              </div>
+            </div>
+            <div className='input-box subscription-text no-border'> Select your subscription plan</div>
+            <div className='input-box'>
+              <div  className='top-offer float'>Offer expired</div>
+              <div className='inputbox-inside'>
+                <div>           
+              <input  className='radio-content' type="radio" id="html" name="fav_language" value="HTML"/>
+              <label for="html"><span className='radio-content'>12 Months Subscription</span></label></div> 
+              <div >
+                <div  className='amount'>Total <span> &#8377;</span>99 </div>
+                <div  className='amount'><span>&#8377;</span>8 /mo</div>
+              </div>
+             
+              </div>
+              </div>  
+            <div className='input-box'>
+            <div className='top-recommended float'>Recommended</div>
+            <div className='inputbox-inside'>
+                <div> 
+              <input  className='radio-content' type="radio" id="html" name="fav_language" value="HTML"/>
+              <label for="html"><span className='radio-content'>12 Months Subscription</span></label> </div>  
+               <div >
+                <div  className='amount'>Total &#8377;179 </div>
+                <div  className='amount'>&#8377;15 /mo</div>
+              </div>
+              </div>
+              </div>
+            <div className='input-box'>
+            <div className='inputbox-inside'>
+                <div> 
+              <input  className='radio-content' type="radio" id="html" name="fav_language" value="HTML"/>
+              <label for="html"><span className='radio-content'>6 Months Subscription</span></label> </div>  
+               <div >
+                <div  className='amount'>Total <span> &#8377;</span>99 </div>
+                <div  className='amount'><span>&#8377;</span>25 /mo</div>
+              </div>
+              </div>
+            </div>
+            <div className='input-box'>
+            <div className='inputbox-inside'>
+                <div> 
+              <input  className='radio-content' type="radio" id="html" name="fav_language" value="HTML"/>
+              <label for="html"><span className='radio-content'>3 Months Subscription</span></label> </div>  
+               <div >
+                <div  className='amount'>Total <span> &#8377;</span>99 </div>
+                <div  className='amount'><span>&#8377;</span>33 /mo</div>
+              </div>
+              </div>
+            </div>
+            <div>
+              <hr className='hr-line'></hr>
+            </div>
+           
+            </div>
+            <div className='below-section'>
+              <div className='below-section-child'>
+                <div className='below-section-child-text'>
+                <div className='radio-content'>Subscription fee</div>
+                <div className='amount' >&#8377;18550</div></div>
+              </div>
+              <div className='below-section-child second-child'>
+                <div className='below-section-child-text'>
+                  <div >
+                <div className='text-child'>Limited time Offer</div>
+                <div className='text-child offer-section'> Offer valid till 25th March 2023</div>
+                  </div>
+                
+                <div className='amount'>-18401</div></div>
+
+              </div>
+              <div className='below-section-child'>
+                <div className='below-section-child-text'>
+                <div  className='amount'>Total(incl.of 18% GST)</div>
+                <div  className='amount'>&#8377; 149</div>
+                </div>
+              </div>
+            </div>
+            <div className='button-section'>
+              <div className='button-container'>
+                <input className='cancel' type='button' value='Cancel'/></div>
+              <div  className='button-container'>
+                <input className='pay' type='button' value='Proceed to Pay'/>
+              </div>
+              </div>
+              <div className='razor'><img src='https://s3-alpha-sig.figma.com/img/0db3/4bae/00b95432c8165078a12780b2eb777818?Expires=1686528000&Signature=m3L7LpqJNnrVNynCy~s44qdWHv94wtFtlVZyWG6Oz4sw8c6RWEICspRXS3ckYJBF-VzbcQ~Kg7Q0jpg0lpIxBn5TAUALkldSryzX2S-MvUftjAEPHRDqf45e3pVzft-OW18E9lQYgLyNUddYKLMvoo0rVx~xDVATciafYC4E02mpOlurCAAd0x8ZV3OIoHS2e0rNOMLr9zaI0~sHxW4n35zg1txw1nTTqGBYJbw32jlhiRXZyXrUudKDnuVB~ys5BK626qLabe66FX-Uhe5qIFKYmUbQ2Mv6JorLKzAuOfe-RpPyxiVoB1RG8w2HJy9y20DGuaA~jePojxj2R0Ap2A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'/></div>
+           
+           </div>
+                   </div>
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
